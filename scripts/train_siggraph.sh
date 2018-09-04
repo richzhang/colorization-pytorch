fe6d2c25a23221afe1d49745c5dde8bd6ac650c6
Richard Zhang <rzhang88@gmail.com>
2018-09-03 18:44:27 -0700

    typo in training script

--- scripts/train_siggraph.sh
+++ scripts/train_siggraph.sh
@@ -3,6 +3,6 @@
 python train.py --name siggraph_class --sample_p 1.0 --niter 15 --niter_decay 0 --classification
 
 # Train regression model (with color hints)
-mkdir ./checkpoints/reg
-cp ./checkpoints/class/latest_net_G.pth ./checkpoints/reg/
+mkdir ./checkpoints/siggraph_reg
+cp ./checkpoints/siggraph_class/latest_net_G.pth ./checkpoints/siggraph_reg/
 python train.py --name siggraph_reg --sample_p .125 --niter 10 --niter_decay 0 --lr 0.00001 --load_model
